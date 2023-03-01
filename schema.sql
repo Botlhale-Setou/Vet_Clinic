fe2964bb2c745e22aa818b9dbd7e0f1036bfa659
Botlhale Setou <botlhale.setou@gmail.com>
2023-03-01 19:55:02 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -33,3 +33,24 @@ ALTER TABLE animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT, ADD CONSTRAINT species_const FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE;
 ALTER TABLE animals ADD COLUMN owner_id INT, ADD CONSTRAINT owners_const FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE;
+
+/*add-join-tables*/
+
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE specializations (
+    species_id INT REFERENCES species(id),
+    vet_id INT REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    date_of_visit DATE
+);
